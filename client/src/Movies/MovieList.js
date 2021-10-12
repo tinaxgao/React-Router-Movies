f8419dfe87c93431f8932ba56d0ef85bc80f03c8
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-11 22:49:38 -0400

    progress

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import {Link, NavLink, useRouteMatch, Route, useParams, useHistory} from 'react-router-dom';
 
 export default function MovieList(props) {
-  return (
+  
+    return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link key={movie.id} to={`/movie/${movie.id}`}>
+        <MovieDetails movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+
+  const history = useHistory();
+  const routeToId = () => {
+    history.push(`/movies/${id}`);
+  };
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={routeToId}>  
+    
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +32,9 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      <Link to={`/movies/${id}`}>Details</Link>
+      <Route path={`/movies/${id}`}>
+      </Route>
     </div>
   );
 }
